3f3d0c106fb0460868fd77f82d6c4b4b1a3f4d1a
hiusnguyen201 <hiusnguyen201@gmail.com>
2024-10-06 03:27:22 +0700

    :zap: (patterns): observer & strategy, (algorithms): push-pull

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+const createError = require("http-errors");
+const path = require("path");
+const p = path.join(__dirname, "../views");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Design Patterns" });
+});
+
+router.get("/patterns/:name", function (req, res, next) {
+  const { name } = req.params;
+  const isValid = fs.existsSync(p + `/patterns/${name}.ejs`);
+  if (!isValid) {
+    return next(createError(404));
+  }
+
+  res.render(`patterns/${name}`, {
+    title: `${name.charAt(0).toUpperCase() + name.slice(1)} Pattern`,
+  });
+});
+
+router.get("/algorithms/:name", function (req, res, next) {
+  const { name } = req.params;
+  const isValid = fs.existsSync(p + `/algorithms/${name}.ejs`);
+  if (!isValid) {
+    return next(createError(404));
+  }
+
+  res.render(`algorithms/${name}`, {
+    title: `${name.charAt(0).toUpperCase() + name.slice(1)} Algorithm`,
+  });
 });
 
 module.exports = router;
